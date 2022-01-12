092930acf32617a91a410cdbb5b277c4c3aa9370
malinhaneberg <94107997+malinhaneberg@users.noreply.github.com>
2022-01-12 18:08:20 +0100

    Add files via upload

---
+++ pizzavino/forretter/java.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navlinks = document.querySelectorAll('.nav_links li');
+
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+    });
+     
+  }
+
+  navSlide();
+
